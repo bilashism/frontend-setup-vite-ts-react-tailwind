f508ccabdde60d01ad5fb09b78da9b02b826ffa4
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2023-07-11 19:15:51 +0600

    testing husky

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# FullBoosts (frontend)  
+# Vite React Tailwind Template
+
+This template provide toolchain below:
+
+- Vite
+- React, ReactDOM
+- ESLint
+- Prettier
+- Git Hook (Husky, Lint Staged)
+- TailwindCSS 3
+
+# Usage
+
+```sh
+pnpm i
+pnpm dev
+```
+
+# Screenshot
+
+![](https://i.imgur.com/ftvsXYB.png)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+// import { checker } from 'vite-plugin-checker'
+// import tsconfigPaths from 'vite-tsconfig-paths'
+
+import reactRefresh from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    reactRefresh(),
+    // tsconfigPaths(),
+    // checker({
+    //   typescript: true,
+    //   enableBuild: true,
+    // }),
+  ],
 })
