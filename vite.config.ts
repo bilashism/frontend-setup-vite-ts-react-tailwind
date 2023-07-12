08e0d8f6709c9048f971ee800be1d570a61c93ad
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2023-07-12 12:48:26 +0600

    chore: https for localhost url

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// for https certificate
+import mkcert from "vite-plugin-mkcert";
+// for localhost url
+import dns from "dns";
+dns.setDefaultResultOrder("verbatim");
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [mkcert(), react()],
+  server: {
+    https: true,
+    host: true,
+    port: 3000
+  }
 });
